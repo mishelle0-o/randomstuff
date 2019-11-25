1e812ca02e75431c00c644a67ad7ea5658586244
mishelle0-o <57626053+mishelle0-o@users.noreply.github.com>
2019-11-25 21:05:56 +0300

    Update circlesbtn.py

--- circlesbtn.py
+++ circlesbtn.py
@@ -12,20 +12,6 @@ class MyWidget(QMainWindow):
         uic.loadUi('UI.ui', self)
         self.btn.clicked.connect(self.run)
         self.show()
-    
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.drawFlag(qp)
-        qp.end()
-
-    def drawFlag(self,qp):
-        qp.setBrush(QColor(255, 0, 0))
-        qp.drawRect(30, 30, 120, 30)
-        qp.setBrush(QColor(0, 255, 0))
-        qp.drawRect(30, 60, 120, 30)
-        qp.setBrush(QColor(0, 0, 255))
-        qp.drawRect(30, 90, 120, 30)
 
     def run(self):
         qp = QPainter()
